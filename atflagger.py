a97caf4ffcc77fa2e7f827f4b55cb277a34631d9
Thomson, Alec (S&A, Kensington) <alec.thomson@csiro.au>
2021-12-03 06:52:42 +0800

    Format

--- atflagger.py
+++ atflagger.py
@@ -11,33 +11,37 @@ import shutil
 import logging as log
 from dask.distributed import LocalCluster, Client
 
+
 def box_filter(spectrum, sigma=3, n_windows=100):
     """
     Filter a spectrum using a box filter.
     """
     # Divide spectrum into windows
-    window_size = len(spectrum)//n_windows
+    window_size = len(spectrum) // n_windows
     dat_filt = np.zeros_like(spectrum).astype(bool)
     # Iterate through windows
     for i in range(n_windows):
-        _dat = spectrum[i*window_size:window_size+i*window_size]
+        _dat = spectrum[i * window_size : window_size + i * window_size]
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             # Use sigma clipping to remove outliers
-            _dat_filt = sigma_clip(_dat, sigma=sigma, maxiters=None, stdfunc=mad_std, masked=True)
-        dat_filt[i*window_size:window_size+i*window_size] = _dat_filt.mask
+            _dat_filt = sigma_clip(
+                _dat, sigma=sigma, maxiters=None, stdfunc=mad_std, masked=True
+            )
+        dat_filt[i * window_size : window_size + i * window_size] = _dat_filt.mask
     return dat_filt
 
-def get_subbands(filename, beam_label='beam_0'):
-    with h5py.File(filename, 'r') as h5:
+
+def get_subbands(filename, beam_label="beam_0"):
+    with h5py.File(filename, "r") as h5:
         # Read header info
-        sb_avail = QTable.read(h5, path=beam_label + '/metadata/band_params')
-    return sb_avail['LABEL']
+        sb_avail = QTable.read(h5, path=beam_label + "/metadata/band_params")
+    return sb_avail["LABEL"]
 
 
-def flag(filename, sb_label, beam_label='beam_0', sigma=3, n_windows=100):
+def flag(filename, sb_label, beam_label="beam_0", sigma=3, n_windows=100):
     # Open HDF5 file
-    with h5py.File(filename, 'r+') as h5:
+    with h5py.File(filename, "r+") as h5:
         # Read header info
         log.info(f"Processing subband {sb_label} - {filename}")
         sb_data = f"{beam_label}/{sb_label}/astronomy_data/data"
@@ -48,21 +52,23 @@ def flag(filename, sb_label, beam_label='beam_0', sigma=3, n_windows=100):
         try:
             flag = np.array(h5[sb_flag])
         except KeyError:
-            raise KeyError(f"No flagging information found for subband {sb_label} - {filename} - run autoflagger first")
+            raise KeyError(
+                f"No flagging information found for subband {sb_label} - {filename} - run autoflagger first"
+            )
 
         f_per = np.sum(flag) / np.sum(np.ones_like(flag)) * 100
         log.info(f"Subband {sb_label} has {f_per:.2f}% flagged - {filename}")
 
         data_xr = xr.DataArray(
-        data, 
-        dims=h5[sb_data].attrs['DIMENSION_LABELS'],
-        coords={
-            'frequency': freq,
-            }
+            data,
+            dims=h5[sb_data].attrs["DIMENSION_LABELS"],
+            coords={"frequency": freq,},
         )
 
         # Set flags
-        data_xr_flg = data_xr.where(~flag[:, np.newaxis,np.newaxis,:,np.newaxis].astype(bool))
+        data_xr_flg = data_xr.where(
+            ~flag[:, np.newaxis, np.newaxis, :, np.newaxis].astype(bool)
+        )
         # Set chunks for parallel processing
         data_xr_flg = data_xr_flg.chunk(
             {
@@ -70,31 +76,33 @@ def flag(filename, sb_label, beam_label='beam_0', sigma=3, n_windows=100):
                 "beam": 1,
                 "polarization": 1,
                 "frequency": len(data_xr.frequency),
-                "bin": 1
-                
+                "bin": 1,
             }
         )
-        mask = xr.apply_ufunc(box_filter, 
-            data_xr_flg, 
-            input_core_dims=[['frequency']], 
-            output_core_dims=[['frequency']],
-            kwargs={'sigma': sigma, 'n_windows': n_windows},
+        mask = xr.apply_ufunc(
+            box_filter,
+            data_xr_flg,
+            input_core_dims=[["frequency"]],
+            output_core_dims=[["frequency"]],
+            kwargs={"sigma": sigma, "n_windows": n_windows},
             dask="parallelized",
             vectorize=True,
-            output_dtypes=(bool)
-            )
+            output_dtypes=(bool),
+        )
 
         # Reduce mask
-        mask_red = mask.sum(dim=('beam','bin','polarization')) > 0
+        mask_red = mask.sum(dim=("beam", "bin", "polarization")) > 0
         log.info(f"Flagging {sb_label} and writing to file...")
         # Write flags back to file
         h5[sb_flag][:] = mask_red.values.astype(int)
 
-        f_per = np.sum(h5[sb_flag].astype(bool)) / np.sum(np.ones_like(h5[sb_flag])) * 100
+        f_per = (
+            np.sum(h5[sb_flag].astype(bool)) / np.sum(np.ones_like(h5[sb_flag])) * 100
+        )
         log.info(f"Subband {sb_label} now has {f_per:.2f}% flagged - {filename}")
 
 
-def main(filenames, beam_label='beam_0', sigma=3, n_windows=100):
+def main(filenames, beam_label="beam_0", sigma=3, n_windows=100):
     # Initialise dask
     cluster = LocalCluster()
     client = Client(cluster)
@@ -103,7 +111,7 @@ def main(filenames, beam_label='beam_0', sigma=3, n_windows=100):
     for filename in filenames:
         log.info(f"Processing file {filename}")
         # Copy hdf5 file
-        new_filename = filename.replace('.hdf', '.atflagged.hdf')
+        new_filename = filename.replace(".hdf", ".atflagged.hdf")
         shutil.copy(filename, new_filename)
 
         log.info(f"Create new file: {new_filename}")
@@ -112,24 +120,38 @@ def main(filenames, beam_label='beam_0', sigma=3, n_windows=100):
 
         # Iterate through subbands
         for sb_label in sb_avail:
-            flag(new_filename, sb_label, beam_label=beam_label, sigma=sigma, n_windows=n_windows)
-        
+            flag(
+                new_filename,
+                sb_label,
+                beam_label=beam_label,
+                sigma=sigma,
+                n_windows=n_windows,
+            )
+
         log.info(f"Finished processing file {filename}")
-    
 
     # Close dask
     client.close()
     cluster.close()
     log.info("Done!")
 
+
 def cli():
     """Command-line interface"""
     import argparse
-    parser = argparse.ArgumentParser(description='Flag SDHDF data')
-    parser.add_argument('filenames', nargs='+', type=str, help='Input SDHDF file(s)')
-    parser.add_argument('--beam', type=str, default='beam_0', help='Beam label')
-    parser.add_argument('--sigma', type=float, default=3, help='Sigma clipping threshold')
-    parser.add_argument('--n_windows', type=int, default=100, help='Number of windows to use in box filter')
+
+    parser = argparse.ArgumentParser(description="Flag SDHDF data")
+    parser.add_argument("filenames", nargs="+", type=str, help="Input SDHDF file(s)")
+    parser.add_argument("--beam", type=str, default="beam_0", help="Beam label")
+    parser.add_argument(
+        "--sigma", type=float, default=3, help="Sigma clipping threshold"
+    )
+    parser.add_argument(
+        "--n_windows",
+        type=int,
+        default=100,
+        help="Number of windows to use in box filter",
+    )
     args = parser.parse_args()
     log.basicConfig(
         level=log.INFO,
@@ -138,5 +160,6 @@ def cli():
     )
     main(args.filenames, args.beam, args.sigma, args.n_windows)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
